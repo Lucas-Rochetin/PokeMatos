97c623f917f4a95325e21e68972de7e7de838668
Elania-Marvers <renneson.baptiste@gmail.com>
2024-10-10 22:43:01 +0200

    [ADD] adding pages

--- main.js
+++ main.js
@@ -16,6 +16,25 @@ const mimeTypes = {
 };
 
 let users = {};  // To store connected users
+const MESSAGE_FILE = './messages.json'; // File to store messages
+
+// Function to load messages from the file
+async function loadMessages() {
+    try {
+        const data = await fs.readFile(MESSAGE_FILE, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        // If file doesn't exist or error reading, return an empty array
+        return [];
+    }
+}
+
+// Function to save a new message
+async function saveMessage(message) {
+    const messages = await loadMessages();
+    messages.push(message);
+    await fs.writeFile(MESSAGE_FILE, JSON.stringify(messages), 'utf8');
+}
 
 const requestListener = function (req, res) {
     const filePath = req.url === '/' ? '/pages/index.html' : req.url;
@@ -37,8 +56,12 @@ const requestListener = function (req, res) {
 const server = http.createServer(requestListener);
 const io = socketIo(server);
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log('A user connected');
+
+    // Send existing messages to the newly connected user
+    const messages = await loadMessages();
+    socket.emit('loadMessages', messages);
 
     // Handle when a username is set
     socket.on('setUsername', (username) => {
@@ -51,8 +74,12 @@ io.on('connection', (socket) => {
     });
 
     // Handle incoming messages
-    socket.on('sendMessage', (message) => {
-        io.emit('message', { username: socket.username, message });
+    socket.on('sendMessage', async (message) => {
+        const fullMessage = { username: socket.username, message };
+        io.emit('message', fullMessage);
+
+        // Save the message to the file
+        await saveMessage(fullMessage);
     });
 
     // Handle user disconnect
